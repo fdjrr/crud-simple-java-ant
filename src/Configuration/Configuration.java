7c457d167c4308bf19cc586ff97701f9b8da3a84
fdjrr <fadjrir.co.id@gmail.com>
2022-01-22 18:03:37 +0700

    first commit

---
+++ src/Configuration/Configuration.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Configuration;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Ayi
+ */
+public class Configuration {
+    public static Connection getConnection() {
+        Connection conn = null;
+        String url = "jdbc:mysql://localhost:3306/test";
+        String user = "root";
+        String password = "";
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return conn;
+    }
+    
+    public static void main(String[] args) {
+        try {
+            Connection c = Configuration.getConnection();
+            System.out.println(String.format("Connected to database %s " + "successfully.", c.getCatalog()));
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+}
